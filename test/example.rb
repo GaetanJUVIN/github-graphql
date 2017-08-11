5b3fc945104524fbb02948ffebd463eadeff0f0a
Caleb Smith <karagenit@outlook.com>
2017-08-11 05:22:40 +0000

    Add More Tests

--- test/example.rb
+++ test/example.rb
@@ -4,7 +4,13 @@ require 'github/graphql'
 require 'test/unit'
 
 class TestQuery < Test::Unit::TestCase
+  
   def test_no_auth
     assert_equal(Github::GraphQL.new(nil, nil, nil).query['message'], 'Bad credentials')
   end
+
+  def test_no_auth_short
+    assert_equal(Github::GraphQL.new(nil, nil).query['message'], 'Bad credentials')
+  end
+
 end
